5fed3e9e79732daa0da61a350dc91f9594ba8858
adiwarsa <adiwarsa03@gmail.com>
2024-11-23 23:16:51 +0800

    first commit

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	/****************ONLY CHANGE THIS****************/
+	'site_name' => 'WA Gateway Multi device version',
+	'header_side' => 'WA Gateway ' . config('app.version'),
+	'footer_name' => 'WA Gateway',
+	'footer_copyright' => '<span class="fs-12">Made with <span class="heart"></span></span>',
+	/************************************************/
+];
